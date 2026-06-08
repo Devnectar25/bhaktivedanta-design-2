d954f6756033e8bc2c505451a398d0641621c8ed
aniketmali-dev <mr.aniketmali21@gmail.com>
2026-06-08 13:05:28 +0530

    Restore admin panel enhancements and remove recovery artifacts

--- scratch/fix.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('src/Admin Panel html code/add-service.html', 'utf8');
-html = html.replace(/\\\$\{/g, '${');
-fs.writeFileSync('src/Admin Panel html code/add-service.html', html);
-console.log('Fixed escaped variables.');
--- scratch/test-runtime.cjs
+++
@@ -1,7 +0,0 @@
-const { JSDOM } = require('jsdom');
-const fs = require('fs');
-const html = fs.readFileSync('src/Admin Panel html code/specialities.html', 'utf8');
-const virtualConsole = new (require('jsdom').VirtualConsole)();
-virtualConsole.on("error", (err) => { console.log("JSDOM Error:", err); });
-virtualConsole.on("jsdomError", (err) => { console.log("JSDOM jsdomError:", err); });
-const dom = new JSDOM(html, { runScripts: "dangerously", virtualConsole });
